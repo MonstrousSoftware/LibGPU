30341b2580599969909a3c7e69881e65244dd719
Monstrous <49096535+MonstrousSoftware@users.noreply.github.com>
2024-10-18 00:05:02 +0200

    added generated enums

--- src/main/java/com/monstrous/wgpu/WGPUSurfaceConfiguration.java
+++ src/main/java/com/monstrous/wgpu/WGPUSurfaceConfiguration.java
@@ -6,13 +6,13 @@ import jnr.ffi.Struct;
 public class WGPUSurfaceConfiguration extends WgpuJavaStruct {
     public final Struct.Pointer nextInChain = new Struct.Pointer();
     public final Struct.Pointer device = new Struct.Pointer();
-    WGPUTextureFormat format;
-    WGPUTextureUsageFlags usage;
-    size_t viewFormatCount;
-    WGPUTextureFormat const * viewFormats;
-    WGPUCompositeAlphaMode alphaMode;
-    uint32_t width;
-    uint32_t height;
-    WGPUPresentMode presentMode;
+//    WGPUTextureFormat format;
+//    WGPUTextureUsageFlags usage;
+//    size_t viewFormatCount;
+//    WGPUTextureFormat const * viewFormats;
+//    WGPUCompositeAlphaMode alphaMode;
+//    uint32_t width;
+//    uint32_t height;
+//    WGPUPresentMode presentMode;
 
 }
