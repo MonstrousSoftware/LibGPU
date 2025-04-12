c3d3a27ec3f91f9360a0f619b5dc592fe8dbc766
Monstrous <49096535+monstroussoftware@users.noreply.github.com>
2025-04-13 00:56:54 +0200

    particle demo with compute shader

--- assets/shaders/particles.wgsl
+++ assets/shaders/particles.wgsl
@@ -3,12 +3,17 @@
 
 struct Uniforms {
     projectionMatrix: mat4x4f,
+    screenSize: vec4f,  //xy
+    quadScale: vec4f,   // xy
+    deltaTime: f32,
 };
 
 struct Particle {
     @location(0) position: vec4f,
     @location(1) velocity : vec4f,
     @location(2) color: vec4f,
+    @location(3) age: f32,      // time to live in seconds
+    @location(4) scale: f32,    // scale factor for particle size
 };
 
 struct Particles {
@@ -19,44 +24,73 @@ struct Particles {
 @group(0) @binding(0) var<uniform> uniforms: Uniforms;
 @group(0) @binding(1) var<storage> dataIn : Particles;
 @group(0) @binding(2) var<storage, read_write> dataOut : Particles;
+@group(0) @binding(3) var texture: texture_2d<f32>;
+@group(0) @binding(4) var textureSampler: sampler;
 
 
 struct VertexOutput {
     @builtin(position) position: vec4f,
     @location(1) velocity : vec4f,
     @location(2) color: vec4f,
+    @location(3) uv: vec2f,
 };
 
+  const localPos = array<vec2f, 6> (
+    // 1st triangle
+    vec2f( 0.0,  0.0),  // center
+    vec2f( 1.0,  0.0),  // right, center
+    vec2f( 0.0,  1.0),  // center, top
+
+    // 2nd triangle
+    vec2f( 0.0,  1.0),  // center, top
+    vec2f( 1.0,  0.0),  // right, center
+    vec2f( 1.0,  1.0),  // right, top
+  );
+
+// use 6 vertex indices per particle
 
 @vertex
 fn vs_main(@builtin(vertex_index) vertexIndex : u32) -> VertexOutput {
    var out: VertexOutput;
 
-   let particle:Particle = dataIn.particles[vertexIndex];
+   let localIx = vertexIndex % 6;
+   let partIx = vertexIndex / 6;
 
-   //out.position = vec4f(0.5, 0.5, 0, 1);// uniforms.projectionMatrix * particle.position;
-   out.position = uniforms.projectionMatrix * particle.position;
+   let particle:Particle = dataIn.particles[partIx];
+
+   let scale:vec2f = particle.scale * uniforms.quadScale.xy;
+   let pos =  uniforms.projectionMatrix * vec4f(particle.position.xy, 0.0, 1.0);
+   out.position = pos + vec4f(localPos[localIx].x*scale.x, localPos[localIx].y*scale.y, 0, 0);
    out.color = particle.color;
+   out.uv.x = localPos[localIx].x;
+   out.uv.y = 1.0 - localPos[localIx].y;
 
    return out;
 }
 
 @fragment
 fn fs_main(in : VertexOutput) -> @location(0) vec4f {
-    return vec4f(in.color);
+    let color =  in.color * textureSample(texture, textureSampler, in.uv);
+    return vec4f(color);
 }
 
 @compute @workgroup_size(8, 8)
 fn updateParticles(@builtin(global_invocation_id) id: vec3<u32>) {
 
    var particle:Particle = dataIn.particles[id.x];
-   particle.position += particle.velocity;
-  //let pos  = uniforms.projectionMatrix * particle.position;
-   particle.velocity.y = select(particle.velocity.y, -particle.velocity.y, particle.position.y < 1 && particle.position.y > 0);
-
-   particle.color.r = length(particle.velocity);
+   particle.position += particle.velocity * uniforms.deltaTime;
+   particle.velocity.y -= 150.0 * uniforms.deltaTime;
+   particle.age -= uniforms.deltaTime;
 
 
+    if (particle.position.x < 0 || particle.position.x > uniforms.screenSize.x) {
+        particle.velocity.x *= -1;
+    }
+    if (particle.position.y < 0 || particle.position.y > uniforms.screenSize.y) {
+         particle.velocity.y *= -0.6;
+    }
+    particle.color.a = clamp(particle.age/2.0, 0.0, 1.0);
+    particle.scale = clamp(particle.age/2.0, 0.0, 1.0);
    dataOut.particles[id.x] = particle;
 
 }
--- tests/src/main/java/com/monstrous/TestParticles.java
+++ tests/src/main/java/com/monstrous/TestParticles.java
@@ -3,6 +3,7 @@ package com.monstrous;
 import com.monstrous.graphics.BitmapFont;
 import com.monstrous.graphics.Color;
 import com.monstrous.graphics.ShaderProgram;
+import com.monstrous.graphics.Texture;
 import com.monstrous.graphics.g2d.SpriteBatch;
 import com.monstrous.graphics.webgpu.*;
 import com.monstrous.math.Matrix4;
@@ -13,12 +14,8 @@ import com.monstrous.webgpu.*;
 import jnr.ffi.Pointer;
 
 // further ideas:
-// convert dots to quads to make them more visible and to add a blended texture
 // emitter on mouse button
-// use delta time
-// make particles wrap around or bounce on screen edges
-// 3d view
-// aging, color change
+
 public class TestParticles extends ApplicationAdapter {
 
     private SpriteBatch batch;
@@ -26,7 +23,7 @@ public class TestParticles extends ApplicationAdapter {
     private long startTime;
     private int frames;
     private String fps;
-
+    private Texture particleTexture;
 
     @Override
     public void create() {
@@ -34,8 +31,12 @@ public class TestParticles extends ApplicationAdapter {
         frames = 0;
         fps = "";
 
+        particleTexture = new Texture("textures/particle.png", false);
+
+
         batch = new SpriteBatch();
         font = new BitmapFont();
+
         initParticles();
     }
 
@@ -44,6 +45,13 @@ public class TestParticles extends ApplicationAdapter {
         if(LibGPU.input.isKeyPressed(Input.Keys.ESCAPE)){
             LibGPU.app.exit();
             return;
+        }
+
+        if(LibGPU.input.isButtonPressed(Input.Buttons.LEFT)){
+            emitParticle( LibGPU.input.getX(), LibGPU.graphics.getHeight()-LibGPU.input.getY(), 100, Color.GREEN_YELLOW, 2.0f, 4.0f);
+        }
+        if(LibGPU.input.isButtonPressed(Input.Buttons.RIGHT)){
+            emitParticle( LibGPU.input.getX(), LibGPU.graphics.getHeight()-LibGPU.input.getY(), 500, Color.RED, 0.05f, 2.0f);
         }
 
         ScreenUtils.clear(Color.BLACK);
@@ -73,6 +81,7 @@ public class TestParticles extends ApplicationAdapter {
     public void resize(int width, int height) {
 
         batch.getProjectionMatrix().setToOrtho2D(0, 0, width, height);
+        screenSize = new Vector3(width, height, 100);
     }
 
     private UniformBuffer uniformBuffer;
@@ -80,53 +89,48 @@ public class TestParticles extends ApplicationAdapter {
     private int flip;
     private Pointer floatData;
     private int numParticles;
+    private int maxParticles;
+    private int writeIndex;
     private int structSize;
     private Pointer computePipeline;
     private ShaderProgram shader;
+    private Vector3 particleScale;
+    private Vector3 screenSize;
 
     private void initParticles(){
 
-        numParticles = 50000;
-        structSize = 12 * Float.BYTES;
+        maxParticles = 8000;
+        numParticles = 0;
+        writeIndex = 0;
+        float speed = 9f;
+        float quadSize = 0.06f;
+        Color color = new Color(Color.YELLOW);
+
+        structSize = 16 * Float.BYTES;
+        float aspectRatio = (float)LibGPU.graphics.getWidth()/LibGPU.graphics.getHeight();
+        particleScale = new Vector3(quadSize, quadSize*aspectRatio, quadSize);
+        screenSize = new Vector3(LibGPU.graphics.getWidth(), LibGPU.graphics.getHeight(), 100);
 
 
-        uniformBuffer = new UniformBuffer(16*Float.BYTES,WGPUBufferUsage.CopyDst | WGPUBufferUsage.Uniform  );
+        uniformBuffer = new UniformBuffer((16+4+4+1)*Float.BYTES,WGPUBufferUsage.CopyDst | WGPUBufferUsage.Uniform  );
 
         particleBuffers = new Buffer[2];
         int usage = WGPUBufferUsage.Storage | WGPUBufferUsage.CopyDst | WGPUBufferUsage.CopySrc;
-        particleBuffers[0] = new Buffer("particles", usage , numParticles * structSize);
-        particleBuffers[1] = new Buffer("next particles", usage , numParticles * structSize);
+        particleBuffers[0] = new Buffer("particles", usage , maxParticles * structSize);
+        particleBuffers[1] = new Buffer("next particles", usage , maxParticles * structSize);
 
         flip = 0;
 
 
         // working buffer in native memory to use as input to WriteBuffer
         floatData = JavaWebGPU.createDirectPointer(structSize);
-        Vector3 pos = new Vector3();
-        Vector3 vel = new Vector3();
-        Color color = new Color(Color.WHITE);
-        for(int i = 0; i < numParticles; i++){
-            pos.set(i * LibGPU.graphics.getWidth()/numParticles, LibGPU.graphics.getHeight()/2, 0);
-            vel.set( 5f*(float)Math.random()-2.5f,5f*(float)Math.random()-2.5f, 0);
 
-            int off = 0;
-            floatData.putFloat(off++*Float.BYTES, pos.x);
-            floatData.putFloat(off++*Float.BYTES, pos.y);
-            floatData.putFloat(off++*Float.BYTES, pos.z);
-            floatData.putFloat(off++*Float.BYTES, 1.0f);
 
-            floatData.putFloat(off++*Float.BYTES, vel.x);
-            floatData.putFloat(off++*Float.BYTES, vel.y);
-            floatData.putFloat(off++*Float.BYTES, vel.z);
-            floatData.putFloat(off++*Float.BYTES, 0f);
+        int W = LibGPU.graphics.getWidth();
+        int H = LibGPU.graphics.getHeight();
 
-            floatData.putFloat(off++*Float.BYTES, color.r);
-            floatData.putFloat(off++*Float.BYTES, color.g);
-            floatData.putFloat(off++*Float.BYTES, color.b);
-            floatData.putFloat(off++*Float.BYTES, color.a);
-
-            particleBuffers[flip].write(i*structSize, floatData, structSize);
-            // should we do one big write instead?
+        for(int i = 0; i < 200 ; i++){
+            emitParticle((float) (Math.random()*W),(float)(Math.random()*H),speed,  color, 1.0f, 2.0f);
         }
 
         shader = new ShaderProgram(Files.internal("shaders/particles.wgsl"));
@@ -136,21 +140,74 @@ public class TestParticles extends ApplicationAdapter {
         computePipeline = makeComputePipeline(shader, pipelineLayout);
     }
 
+    private void emitParticle(float x, float y, float speed, Color color, float scale, float age){
+        if(writeIndex >= maxParticles)
+            writeIndex = 0; // start overwriting oldest particles
+
+        Vector3 pos = new Vector3();
+        Vector3 vel = new Vector3();
+
+        pos.set(x, y,0);
+        float angle = (float)(2*Math.PI * Math.random());
+        vel.set( speed*(float)(Math.cos(angle)),speed*((float)Math.sin(angle)), 0);
+
+
+        int off = 0;
+        floatData.putFloat(off++*Float.BYTES, pos.x);
+        floatData.putFloat(off++*Float.BYTES, pos.y);
+        floatData.putFloat(off++*Float.BYTES, pos.z);
+        floatData.putFloat(off++*Float.BYTES, 1.0f);
+
+        floatData.putFloat(off++*Float.BYTES, vel.x);
+        floatData.putFloat(off++*Float.BYTES, vel.y);
+        floatData.putFloat(off++*Float.BYTES, vel.z);
+        floatData.putFloat(off++*Float.BYTES, 0f);
+
+        floatData.putFloat(off++*Float.BYTES, color.r);
+        floatData.putFloat(off++*Float.BYTES, color.g);
+        floatData.putFloat(off++*Float.BYTES, color.b);
+        floatData.putFloat(off++*Float.BYTES, color.a);
+
+        floatData.putFloat(off++*Float.BYTES, age);
+        floatData.putFloat(off++*Float.BYTES, scale);
+
+        // pad to a multiple of 16
+        floatData.putFloat(off++*Float.BYTES, 0);
+        floatData.putFloat(off++*Float.BYTES, 0);
+
+        particleBuffers[flip].write(writeIndex*structSize, floatData, structSize);
+        writeIndex++;
+        if(writeIndex > numParticles)
+            numParticles = writeIndex;
+        // should we do one big write instead?
+    }
+
+
 
     private void renderParticles(){
-        setUniforms(batch.getProjectionMatrix());   // PV matrix?
+
+        Matrix4 projectionMatrix = new Matrix4();
+        projectionMatrix.setToOrtho(0f, LibGPU.graphics.getWidth(), 0f, LibGPU.graphics.getHeight(), -1f, 1f);
+        setUniforms(batch.getProjectionMatrix(), screenSize, particleScale, LibGPU.graphics.getDeltaTime());   // PV matrix?
 
         RenderPass renderPass = RenderPassBuilder.create(null, LibGPU.app.configuration.numSamples);
 
         BindGroupLayout bindGroupLayout = createBindGroupLayout();
-        BindGroup bg = makeBindGroup(bindGroupLayout, uniformBuffer, particleBuffers[flip], particleBuffers[1-flip]);
+        BindGroup bg = makeBindGroup(bindGroupLayout, uniformBuffer, particleBuffers[flip], particleBuffers[1-flip], particleTexture);
 
         PipelineLayout pipelineLayout = new PipelineLayout("particle render pipeline", bindGroupLayout);
 
         PipelineSpecification pipeSpec = new PipelineSpecification();
         pipeSpec.numSamples =  LibGPU.app.configuration.numSamples;
-        pipeSpec.topology = WGPUPrimitiveTopology.PointList;
+        pipeSpec.topology = WGPUPrimitiveTopology.TriangleList;
         pipeSpec.shader = shader;
+        pipeSpec.blendSrcColor = WGPUBlendFactor.SrcAlpha;
+        pipeSpec.blendDstColor = WGPUBlendFactor.One;
+        pipeSpec.blendOpColor = WGPUBlendOperation.Add;
+        pipeSpec.blendSrcAlpha = WGPUBlendFactor.Src;
+        pipeSpec.blendDstAlpha = WGPUBlendFactor.One;
+        pipeSpec.blendOpAlpha = WGPUBlendOperation.Add;
+        pipeSpec.disableDepthTest();    // disable depth test to avoid transparent pixels appearing as black
 
         Pipeline pipeline = new Pipeline(pipelineLayout.getHandle(), pipeSpec);
 
@@ -158,13 +215,14 @@ public class TestParticles extends ApplicationAdapter {
         renderPass.setBindGroup( 0, bg.getHandle(), 0, JavaWebGPU.createNullPointer());
 
 
-        renderPass.draw( numParticles );
+        renderPass.draw( 6* numParticles ); // 6 vertices per particle
         bg.dispose();
         renderPass.end();
 
     }
 
     private void updateParticles(){
+        setUniforms(batch.getProjectionMatrix(), screenSize, particleScale, 0.01f); //LibGPU.graphics.getDeltaTime());   // PV matrix?
 
         // create a command encoder
         CommandEncoder encoder = new CommandEncoder(LibGPU.device);
@@ -174,7 +232,7 @@ public class TestParticles extends ApplicationAdapter {
         pass.setPipeline(computePipeline);
 
         BindGroupLayout bindGroupLayout = createBindGroupLayout();
-        BindGroup bindGroup = makeBindGroup(bindGroupLayout, uniformBuffer, particleBuffers[flip], particleBuffers[1-flip]);
+        BindGroup bindGroup = makeBindGroup(bindGroupLayout, uniformBuffer, particleBuffers[flip], particleBuffers[1-flip], particleTexture);
         flip = 1 - flip;
         // set bind group
         pass.setBindGroup(0, bindGroup);
@@ -199,31 +257,38 @@ public class TestParticles extends ApplicationAdapter {
     }
 
 
-    private void setUniforms(Matrix4 projectionMatrix){
+    private void setUniforms(Matrix4 projectionMatrix, Vector3 enclosureSize, Vector3 scale, float deltaTime){
         uniformBuffer.beginFill();
         uniformBuffer.append(projectionMatrix);
+        uniformBuffer.append(enclosureSize);
+        uniformBuffer.append(scale);
+        uniformBuffer.append(deltaTime);
         uniformBuffer.endFill();
     }
 
     private BindGroupLayout createBindGroupLayout() {
         BindGroupLayout layout = new BindGroupLayout("particle render bind group layout");
         layout.begin();
-        layout.addBuffer(0, WGPUShaderStage.Vertex, WGPUBufferBindingType.Uniform, uniformBuffer.getSize(), false);
+        layout.addBuffer(0, WGPUShaderStage.Vertex|WGPUShaderStage.Compute, WGPUBufferBindingType.Uniform, uniformBuffer.getSize(), false);
         // note: vertex shader can only be bound to READ-ONLY storage buffers
         layout.addBuffer(1, WGPUShaderStage.Vertex|WGPUShaderStage.Compute, WGPUBufferBindingType.ReadOnlyStorage, particleBuffers[0].getSize(), false);
         // we ping-pong between 2 buffers for the compute shader
         layout.addBuffer(2, WGPUShaderStage.Compute, WGPUBufferBindingType.Storage, particleBuffers[0].getSize(), false);
+        layout.addTexture(3, WGPUShaderStage.Fragment, WGPUTextureSampleType.Float, WGPUTextureViewDimension._2D, false);
+        layout.addSampler(4,  WGPUShaderStage.Fragment, WGPUSamplerBindingType.Filtering);
         layout.end();
         return layout;
     }
 
 
-    private BindGroup makeBindGroup(BindGroupLayout bindGroupLayout, Buffer uniformBuffer, Buffer in, Buffer out) {
+    private BindGroup makeBindGroup(BindGroupLayout bindGroupLayout, Buffer uniformBuffer, Buffer in, Buffer out, Texture texture) {
         BindGroup bg = new BindGroup(bindGroupLayout);
         bg.begin();
         bg.addBuffer(0, uniformBuffer);
         bg.addBuffer(1, in);
         bg.addBuffer(2, out);
+        bg.addTexture(3, texture.getTextureView());
+        bg.addSampler(4, texture.getSampler());
         bg.end();
         return bg;
     }
